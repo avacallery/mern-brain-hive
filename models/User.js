e7aa07cd5a44f554051350cdd179dbf38f73e4bc
avacallery <avacallery@gmail.com>
2020-06-08 12:56:27 -0400

    add User model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    lastLogin: Date,
+  },
+  //timestamps(updated at and created at) will track changes to data
+  //mongoose will handle the timestamps
+  { timestamps: {} }
+);
+
+module.exports = User = mongoose.model('users', userSchema);
