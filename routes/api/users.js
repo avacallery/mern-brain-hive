4663c6089bc70a0e421c6f67c33c4d88e6e6b971
avacallery <avacallery@gmail.com>
2020-06-09 10:44:45 -0400

    add validation function to post route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
 //when we use () after a function, we are executing express.Router() and creating an instance aka const router
 //this isn't the root directory so we bring in router
 
@@ -8,7 +9,15 @@ const router = express.Router();
 // @access   Public
 
 router.post('/', (req, res) => {
-  res.json({ body: req.body });
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  res.send(user);
 });
 
 //create branch with validation
@@ -16,5 +25,13 @@ router.post('/', (req, res) => {
 //must have email and password
 //email must be valid
 //password must have at least 6 characters
+
+function validateUser(user) {
+  const schema = {
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+  };
+  return Joi.validate(user, schema);
+}
 
 module.exports = router;
