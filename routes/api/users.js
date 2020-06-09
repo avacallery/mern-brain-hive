8db68e7acce26050563b8b8728a0475d6ab7ea90
avacallery <avacallery@gmail.com>
2020-06-09 12:37:32 -0400

    add validation to POST user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
 const router = express.Router();
+const isEmpty = require('../../utilities/isEmpty');
 
 // @route    POST api/users/
 // @desc     create new user
@@ -8,8 +9,18 @@ const router = express.Router();
 
 router.post(
   '/',
-  [check('email', 'Email is required').not().isEmpty()],
+  [
+    check('email', 'Email required').not().isEmpty(),
+    check('email', 'Valid email required').isEmail(),
+    check('password', 'Password must contain at least 6 characters').isLength({
+      min: 6,
+    }),
+  ],
   (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     res.json({ body: req.body });
   }
 );
