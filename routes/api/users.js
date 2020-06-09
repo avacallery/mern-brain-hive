69b4b619e3859dd5d2b53ca0bc96b256b0747cd9
avacallery <avacallery@gmail.com>
2020-06-09 12:24:58 -0400

    create isEmpty function

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,20 +1,17 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
-//when we use () after a function, we are executing express.Router() and creating an instance aka const router
-//this isn't the root directory so we bring in router
 
 // @route    POST api/users/
 // @desc     create new user
 // @access   Public
 
-router.post('/', (req, res) => {
-  res.json({ body: req.body });
-});
-
-//create branch with validation
-//validate the body of the request
-//must have email and password
-//email must be valid
-//password must have at least 6 characters
+router.post(
+  '/',
+  [check('email', 'Email is required').not().isEmpty()],
+  (req, res) => {
+    res.json({ body: req.body });
+  }
+);
 
 module.exports = router;
