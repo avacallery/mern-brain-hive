e848ee6681d28de7c167a2b39ad04f91ce97a1c5
avacallery <avacallery@gmail.com>
2020-07-07 12:47:31 -0400

    create POST model

--- models/Post.js
+++ models/Post.js
@@ -17,59 +17,64 @@ const commentSchema = new Schema(
   { timestamps: {} }
 );
 
-const postSchema = new Schema({
-  poster: {
-    //check profiles model to find id
-    type: Schema.Types.ObjectId,
-    ref: 'profiles',
-    required: true,
+const postSchema = new Schema(
+  {
+    poster: {
+      //check profiles model to find id
+      type: Schema.Types.ObjectId,
+      ref: 'profiles',
+      required: true,
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    author: String,
+    skillLevel: {
+      type: String,
+      //enum gives an array of strings that will only be allowed
+      enum: [
+        'Beginner',
+        'Intermediate',
+        'Advanced',
+        'Associate',
+        'Junior',
+        'Senior',
+        'Lead',
+      ],
+    },
+    cohort: String,
+    title: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: [String],
+      default: [],
+    },
+    summary: String,
+    link: String,
+    resourceType: {
+      type: String,
+      enum: [
+        'Other',
+        'Article',
+        'Video',
+        'Book',
+        'Slideshow',
+        'eBook',
+        'podcast',
+      ],
+    },
+    publishedAt: Date,
+    videoLength: Number,
+    timeToComplete: Number,
+    comments: { type: [commentSchema], default: [] },
+    // TODO rating
+    // TODO archive
+    // TODO feature
   },
-  deleted: {
-    type: Boolean,
-    default: false,
-  },
-  author: String,
-  skillLevel: {
-    type: String,
-    //enum gives an array of strings that will only be allowed
-    enum: [
-      'Beginner',
-      'Intermediate',
-      'Advanced',
-      'Associate',
-      'Junior',
-      'Senior',
-      'Lead',
-    ],
-  },
-  cohort: String,
-  title: {
-    type: String,
-    required: true,
-  },
-  categories: {
-    type: [String],
-    default: [],
-  },
-  summary: String,
-  link: String,
-  resourceType: {
-    type: String,
-    enum: [
-      'Other',
-      'Article',
-      'Video',
-      'Book',
-      'Slideshow',
-      'eBook',
-      'podcast',
-    ],
-  },
-  publishedAt: Date,
-  videoLength: Number,
-  timeToComplete: Number,
-  comments: { type: [commentSchema], default: [] },
-  // TODO rating
-  // TODO archive
-  // TODO feature
-});
+  { timestamps: {} }
+);
+
+module.exports = Post = mongoose.model('posts', postSchema);
