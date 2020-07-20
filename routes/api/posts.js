897f4fe39bf8a448e58197614a2f3f3eb58415f8
avacallery <avacallery@gmail.com>
2020-07-20 13:56:08 -0400

    add Post route with validation

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,6 +4,7 @@ const { check, validationResult } = require('express-validator');
 
 const auth = require('../../middleware/auth');
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
 // @route   GET api/posts/test
@@ -29,6 +30,19 @@ router.post(
     check('author', 'Author is required').notEmpty(),
     check('title', 'Title is required').notEmpty(),
     check('skillLevel', 'Select from dropdown').isIn([
+      'Beginner',
+      'Intermediate',
+      'Advanced',
+      'Associate',
+      'Junior',
+      'Senior',
+      'Lead',
+    ]),
+    check('summary', 'Summary is required').notEmpty(),
+    check('link', 'Need link').notEmpty(),
+    check('link', 'Valid URL required').isURL(),
+    check('resourceType', 'Need resource type').notEmpty(),
+    check('resourceType', 'Select from dropdown').isIn([
       'Other',
       'Article',
       'Video',
@@ -37,24 +51,39 @@ router.post(
       'eBook',
       'podcast',
     ]),
-    check('summary', 'Summary is required').notEmpty(),
-    check('link', 'Valid URL required').optional().isURL(),
+    check('publishedAt', 'Date of resource required').optional().isISO8601(),
+    check('videoLength', 'Round video length to closest minute')
+      .optional()
+      .isNumeric()
+      .isInt(),
+    check('timeToComplete', 'Round complete time to closest minute')
+      .optional()
+      .isNumeric(),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    return res.json(req.body);
+    try {
+      // store postData in variable
+      const postData = req.body;
+      console.log(postData);
+      // get profile matching userId using req.user.id
+      const profile = await Profile.findOne({ user: req.user.id });
+      // add profileId to postData
+      postData.poster = profile.id;
+      // create the post in the database
+      // postData we have updated with our id
+      // const post = await Post.create(postData);
+      return res.json(await Post.create(postData));
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
   }
 );
 
-// resourceType -> string -> enum
-// publishedAt -> date -> optional
-// videoLength -> number -> optional
-// timeToComplete -> number -> optional
-
-// use express-validator to check if data is valid/right format
 // Post model -> post to the Post model in db
 // return the new Post to the frontend
 
