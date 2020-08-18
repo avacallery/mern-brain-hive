fd21361bf90e86bd76e1c3d0b534804635007a41
avacallery <avacallery@gmail.com>
2020-08-18 12:38:16 -0400

    concurrently

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.use('/api/users', users);
 app.use('/api/profiles', profiles);
 app.use('/api/posts', posts);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
